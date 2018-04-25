5307042dcd7eef38e856700050bb2ac34741f83c
Vitaly Mes <volkondr@ya.ru>
2018-04-25 12:44:29 +0300

    Добавляет меню

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,15 +1,13 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+var navWrapper = document.querySelector('.main-nav__wrapper');
 navMain.classList.add('main-nav--closed');
 navMain.classList.remove('main-nav--opened');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
+  navWrapper.classList.toggle('main-nav__wrapper--closed');
+  navWrapper.classList.toggle('main-nav__wrapper--opened');
 });
